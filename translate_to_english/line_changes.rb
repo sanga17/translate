6a0984cc8cbb99388d4d60502f7306a4df4fc46b
sanga17 <sangmesh.ausekar@wgblindia.com>
2018-03-15 13:26:58 +0530

    logic is separeted in different classes

--- translate_to_english/line_changes.rb
+++ translate_to_english/line_changes.rb
@@ -1,12 +1,36 @@
+require_relative 'word_changes'
+GLOSARY_FILE = "glosary.txt"
 class LineChanges
+
   def initialize(line)
     @line = line
   end
-  def self.last_word_correction()
-    word_array = line.split(' ')
-    word_array.each do |word|
 
+  def last_word_correction
+    word_array = @line.split(' ')
+    word_array.each_with_index do |word , index|
+      word_object = WordChanges.new(word)
+      word = word_object.remove_special_character
+      word = word_object.last_word_remover
+      word_array[index] = word
     end
-    line
+    @line = word_array.reject(&:empty?).join(' ')
+    puts @line
   end
+
+  def line_replace_with
+    line_converted = ''
+    glosary = File.read(GLOSARY_FILE)
+    glosary.each_line do |line1|
+      line1 = line1.chomp
+      hash = Hash[*line1.split(" ")]
+      hash.each do |key,value|
+        if @line.include? key
+          line_converted = @line.gsub!(key,value)
+        end
+      end
+    end
+    line_converted
+  end
+
 end
--- translate_to_english/translate_to_english.rb
+++ translate_to_english/translate_to_english.rb
@@ -1,10 +1,11 @@
-GLOSARY_FILE = "glosary.txt"
+require_relative 'line_changes'
 class TranslateToEnglish
   def initialize
     @input_file
     @out_file
     @new_file
     @folder_path
+    @text_file
   end
 
 
@@ -19,76 +20,44 @@ class TranslateToEnglish
     puts "completed"
   end
 
-  def self.create_file(ext)
-      text_file = File.new(@input_file)
-      @new_file = File.basename(@input_file)
-      @out_file = File.new("#{OP_FOLDER_PATH}/#{@new_file}", "w+")
+  def create_file(ext)
+    @text_file = File.new(@input_file)
+    @new_file = File.basename(@input_file)
+    @out_file = File.new("#{@folder_path}/#{@new_file}", "w+")
   end
 
-  def self.file_transfer(text_file,out_file)
+  def file_transfer(text_file,out_file)
     line_converted = ""
     text_file.each_line do |line|
-      line = TranslateToEnglish.remove_character(line)
-      line = TranslateToEnglish.last_word_correction(line)
-      glosary = File.read(GLOSARY_FILE)
-      glosary.each_line do |line1|
-        line1 = line1.chomp
-        hash = Hash[*line1.split(" ")]
-        hash.each do |key,value|
-          if line.include? key
-            line_converted = line.gsub!(key,value)
-          end
-        end
-      end
+      line_object = LineChanges.new(line)
+      line_converted = line_object.last_word_correction
+      line_converted = line_object.line_replace_with
       out_file.puts (line_converted)
     end
   end
 
-  def self.convert
-    File.open('some_file', 'r') do |file|
-      file.each_line do |line|
-        line_converted = line.gsub!(key,value)
-        out_file.puts(line_converted)
-      end
-    end
-  end
-
-  def self.take_file_path
+  def take_file_path
     puts "enter file:"
     @input_file = gets.chomp
     puts "enter file path to store resulting file"
     @folder_path = gets.chomp
     ext = File.extname(@input_file)
-    TranslateToEnglish.conversion_of_file(ext)
+    conversion_of_file(ext)
   end
 
-  def self.conversion_of_file(ext)
-    TranslateToEnglish.create_file(ext)
-    TranslateToEnglish.file_transfer(@text_file,@out_file)
+  def conversion_of_file(ext)
+    create_file(ext)
+    file_transfer(@text_file,@out_file)
     puts "the resulting file is converted here:
     #{@folder_path}/#{@new_file}"
-    TranslateToEnglish.close_operation
+    close_operation
   end
 
-  def self.close_operation
+  def close_operation
     @text_file.close
     @out_file.close
   end
 
-
-
-  def self.remove_character(line)
-    word_array = line.split(' ')
-    word_array.each do |word|
-      if word.include? "'"
-        word1 = word.gsub("'" , "")
-        line.sub! word, word1
-        puts word1
-      end
-    end
-    line
-  end
-
 end
 t = TranslateToEnglish.new
-TranslateToEnglish.take_file_path
+t.take_file_path
